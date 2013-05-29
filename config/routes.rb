abdf7aaea615c4b87d9b0aa4efdb625b10205bd4
Michael Raimondi <zeno@pobox.com>
2013-05-29 16:52:28 -0400

    Add user model and associated devise infrastructure

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 LaunchedIt::Application.routes.draw do
+  devise_for :users
+
+  root :to => "main_pages#home" 
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
